c7368506917f4b20c50e43a28bddf9136a37212f
Luuukeee08 <108565244+Luuukeee08@users.noreply.github.com>
2023-02-13 09:15:03 +0900

    memo

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function($) {
+  $('.firstview').bgSwitcher({
+      images: ['images/flower.png','images/gold.png'],
+      interval: 2500,
+      loop: true
+  });
+
+  $(window).scroll(function () {
+    var targetElement = $('.fadein').offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > targetElement - windowHeight + 200) {
+      $('.fadein').css('opacity', '1');
+      $('.fadein').css('transform', 'translateY(0)');
+    }
+  });
+});
